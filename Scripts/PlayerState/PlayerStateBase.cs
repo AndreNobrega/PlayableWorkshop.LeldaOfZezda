663402790558093a967aa2fba1cf59d2d9298698
AndreN <andre.ds.nobrega@gmail.com>
2026-03-19 16:56:53 +0100

    added player states

--- Scripts/PlayerState/PlayerStateBase.cs
+++ Scripts/PlayerState/PlayerStateBase.cs
@@ -5,13 +5,13 @@ using System;
 public partial class PlayerStateBase : RefCounted
 {
 	// Called when we first enter this state.
-	public void Enter(){}
+	public void Enter(Player player){}
 
 	// Called when we exit this state.
-	public void Exit(){}
+	public void Exit(Player player){}
 
 	// Called for every physics frame that we're in this state.
-	public void Update(float delta)
+	public void Update(Player player, double delta)
 	{
 		return;
 	}
